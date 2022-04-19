a0db37e378ca5dd6fd7d248652d2f49e57f88500
dhjsdhned <102592062+dhjsdhned@users.noreply.github.com>
2022-04-19 16:25:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,173 @@
+//declaring variables
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+var restartImg, gameOverImg, Restart, GAMEOVER;
+
+var score;
+
+
+// loading the images of different sprites
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+
+  restartImg=loadImage("restart.png");;
+  gameOverImg=loadImage("gameOver.png");
+  
+  
+}
+//creating the sprites for different objects in the game
+//adding animation or images to the respective sprites
+//using scale property to maintain the size of the sprites 
+//making the invisible ground false so that the trex is seen walking on the original ground
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided" , trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+
+  Restart=createSprite(300,140);
+  Restart.addImage("Restart", restartImg);
+  Restart.scale=0.5;
+  Restart.visible=false;
+
+  GAMEOVER=createSprite(300,100);
+  GAMEOVER.addImage("GameOver", gameOverImg);
+  GAMEOVER.scale=0.7;
+  GAMEOVER.visible=false;
+
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
+  
+  console.log("Hello" + 5);
+  
+  score = 0;
+}
+
+function draw() {
+  background(180);
+  text("Score: "+ score, 500,50);
+  
+  
+  // this formulates the code and allows it to go in a particular sequence and should only be played while the game is being played
+  if(gameState === PLAY){
+       
+    ground.velocityX = -4;
+   
+    score = score + Math.round(frameCount/60);
+    
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    
+   
+    if(keyDown("space")&& trex.y >= 100) {
+        trex.velocityY = -13;
+    }
+    
+  
+    trex.velocityY = trex.velocityY + 0.8
+  
+  
+    spawnClouds();
+  
+   
+    spawnObstacles();
+    
+    if(obstaclesGroup.isTouching(trex)){
+        gameState = END;
+    }
+  }
+  // this formulates the code and allows it to go in a particular sequence and should only be played while the game has ended
+   else if (gameState === END) {
+       
+      ground.velocityX = 0;
+      Restart.visible=true;
+      GAMEOVER.visible=true;
+     
+     obstaclesGroup.setVelocityXEach(0);
+     cloudsGroup.setVelocityXEach(0);
+   }
+  
+ 
+
+  trex.collide(invisibleGround);
+  
+  
+  
+  drawSprites();
+}
+// we are spawning obstacles and making sure that the obstacles come in a random order rather than a sequence
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   var obstacle = createSprite(400,165,10,40);
+   obstacle.velocityX = -6;
+   
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+   
+    obstaclesGroup.add(obstacle);
+ }
+}
+// we are spawning clouds and making sure that the clouds don't all appear at the same time, but at different times and random places
+function spawnClouds() {
+   if (frameCount % 60 === 0) {
+     cloud = createSprite(600,100,40,10);
+    cloud.y = Math.round(random(10,60));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+    cloud.lifetime = 134;
+    
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+   cloudsGroup.add(cloud);
+    }
+}
+
